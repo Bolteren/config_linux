d5b9d9db9354e280a9995532a2cb8a6c179bd667
bolter <bolter@gmail.com>
2023-12-16 21:37:09 +0500

    refactoring and add end menu

--- 1.bash
+++ 1.bash
@@ -16,11 +16,13 @@ settings_bashrc()
     sed -i 's/#alias la=/alias la=/' /home/$SUDO_USER/.bashrc
     sed -i 's/#alias ls=/alias ls=/' /home/$SUDO_USER/.bashrc
 }
+
 cp_bashrc_all()
 {
     msg_info "copy bashrc all new users"
     cp /home/$SUDO_USER/.bashrc /etc/skel/.bashrc
 }
+
 cp_vimrc_all()
 {
     msg_info "copy vimrc all new users"
@@ -41,6 +43,7 @@ settings_vimrc()
 	echo "set expandtab" >> /home/$SUDO_USER/.vimrc
     echo ":map <F5> <Esc>:wq<Enter>" >> /home/$SUDO_USER/.vimrc 
 }
+
 msg_info() {
 	echo -en "\033[33;1;44m"
     clear
@@ -49,6 +52,7 @@ msg_info() {
     local msg="$1"
 	echo -ne " ${HOLD} ${YW}${msg}...\n"
 }
+
 msg_error()
 {
 	local msg="$1"
@@ -60,21 +64,23 @@ msg_default()
     tput sgr0
     clear
 }
-clear
-if [[ $EUID -ne 0 ]]; then
-	msg_error "Данный скрипт работает только с правами суперпользователя"
-	exit 1
-fi
 
-RESULT=$(apt | tr -s '\r\n' ' ' | cut -d ' ' -f1)
-if [[ "$RESULT" == "apt" ]]; then
-	msg_info "update sources.list"
+settings_main()
+{
+    clear
+        if [[ $EUID -ne 0 ]]; then
+            msg_error "Данный скрипт работает только с правами суперпользователя"
+            exit 1
+        fi
+
+    RESULT=$(apt | tr -s '\r\n' ' ' | cut -d ' ' -f1)
+    if [[ "$RESULT" == "apt" ]]; then
+        msg_info "update sources.list"
         sed -i.bak 's/deb cdrom/#deb cdrom/' /etc/apt/sources.list
-	CHOICE=$(whiptail --title "Vim" --menu "Сделать основным редактором Vim или nano" 14 58 2 "vim" " " \
+        CHOICE=$(whiptail --title "Vim" --menu "Сделать основным редактором Vim или nano" 14 58 2 "vim" " " \
 	       	"nano" " " 3>&2 2>&1 1>&3)
-	case $CHOICE in
-		vim)
-
+        case $CHOICE in
+        vim)
 			msg_info "Update"
 			apt update &>/dev/null
 			msg_info "remove nano"
@@ -98,13 +104,16 @@ if [[ "$RESULT" == "apt" ]]; then
             cp_bashrc_all
             msg_default
 			;;
-	esac
-    msg_info "UPGRADE"
-    apt dist-upgrade -y 1>/dev/null 2>/dev/null
-    apt autoremove 1>/dev/null 2>/dev/null
-    apt autoclean 1>/dev/null 2>/dev/null
+        esac
+            msg_info "UPGRADE"
+            apt dist-upgrade -y 1>/dev/null 2>/dev/null
+            apt autoremove 1>/dev/null 2>/dev/null
+            apt autoclean 1>/dev/null 2>/dev/null
+    else
+        echo "Using non apt"
+    fi
+    whiptail --title "Настройка" --msgbox "Настройка завершина" 14 58
+    msg_default
+}
 
-else
-	echo "Using non apt"
-fi
-msg_default
+settings_main
